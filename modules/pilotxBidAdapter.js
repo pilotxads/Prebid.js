1dadffb7abb6fc45a1ee6c6f25a30840490a4ad3
Desmond <desmond@bluewaterads.com>
2020-07-27 15:44:04 -0700

    video stuff

--- modules/pilotxBidAdapter.js
+++ modules/pilotxBidAdapter.js
@@ -7,6 +7,7 @@ import { registerBidder } from '../src/adapters/bidderFactory.js';
 // pbjs.getBidResponses(); check bidresponse in console
 // https://git.online-solution.biz/osi/prebidjs/commit/10ec9a599f46502b14ef41bf09173c36c959d7d5
 // http://localhost:9999/integrationExamples/gpt/pbjs_video_adUnit.html?pbjs_debug=true&pbjs_testbids=true
+// http://localhost:9999/integrationExamples/gpt/hello_world.html?pbjs_debug=true&pbjs_testbids=true
 const BIDDER_CODE = 'pilotx';
 const ENDPOINT_URL = 'http://sparta.bwaserver.com/hb';
 const CURRENCY = 'USD';
@@ -25,7 +26,7 @@ export const spec = {
     if (!bid.params.placementId) {
       return false;
     }
-    if (bid.mediaTypes.hasOwnProperty('banner') && bid.sizes.length < 1) {
+    if (!bid.sizes.length) {
       return false;
     }
 
@@ -39,6 +40,7 @@ export const spec = {
     utils._each(validBidRequests, function(bid) {
       utils.logInfo('== PILOTX 3== ', bid)
       // bid.sizes = [[300, 250], [300, 600]];
+      // bid.sizes = [[640, 480]];
       utils.logInfo('== PILOTX 4== ', bid.sizes)
       payloadItems[1] = [bid.sizes[0][0], bid.sizes[0][1], bid.bidId]
     });
@@ -66,7 +68,20 @@ export const spec = {
 
       utils._each(response, function(bidResponse) {
         if (!bidResponse.is_passback) {
+          // if ('banner' in imp) {
+          //   prBid.mediaType = BANNER;
+          //   prBid.width = rtbBid.w;
+          //   prBid.height = rtbBid.h;
+          //   prBid.ad = formatAdMarkup(rtbBid);
+          // } else if ('video' in imp) {
+          //   prBid.mediaType = VIDEO;
+          //   prBid.vastUrl = rtbBid.nurl;
+          //   prBid.width = imp.video.w;
+          //   prBid.height = imp.video.h;
+          // }
+
           bidResponses.push({
+            mediaType: VIDEO,
             requestId: requested[2],
             cpm: bidResponse.price,
             width: requested[0],
@@ -75,7 +90,9 @@ export const spec = {
             currency: CURRENCY,
             netRevenue: false,
             ttl: TIME_TO_LIVE,
-            ad: bidResponse.adm
+            // ad: bidResponse.adm
+            // ad: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+            vastUrl: `http://localhost:9999/integrationExamples/gpt/vast.xml`
           });
         }
       });
